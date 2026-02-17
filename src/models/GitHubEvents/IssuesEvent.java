d9bf760e5babe5f209f4f49c4c31d24bd394f2d2
FSXplay <skinoftheduck@gmail.com>
2026-02-17 17:54:37 +0700

    Change naming convention in IssuesEvent

--- src/models/GitHubEvents/IssuesEvent.java
+++ src/models/GitHubEvents/IssuesEvent.java
@@ -2,21 +2,21 @@ package models.GitHubEvents;
 
 import java.time.OffsetDateTime;
 
-enum Action {
+enum IssueAction {
     OPENED,
     CLOSED,
     REOPENED
 }
 
 public class IssuesEvent extends GitHubEvent {
-    private Action action;
+    private IssueAction action;
 
-    public IssuesEvent(String _repoName, Action _action, OffsetDateTime _createdAt) {
+    public IssuesEvent(String _repoName, IssueAction _action, OffsetDateTime _createdAt) {
         super("IssuesEvent", _repoName, _createdAt);
         this.action = _action;
     }
 
-    public Action geAction() { return this.action; }
+    public IssueAction geAction() { return this.action; }
 
     @Override
     public String toString() {
