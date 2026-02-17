07efeb01ffc5b59dbf1ead5f36b3f7f164616c53
FSXplay <skinoftheduck@gmail.com>
2026-02-17 18:07:13 +0700

    Model of PullRequestReviewCommentEvent

---
+++ src/models/GitHubEvents/PullRequestReviewCommentEvent.java
@@ -0,0 +1,19 @@
+package models.GitHubEvents;
+
+import java.time.OffsetDateTime;
+
+public class PullRequestReviewCommentEvent extends GitHubEvent {
+    private int number;
+    
+    public PullRequestReviewCommentEvent(String _repoName, OffsetDateTime _createdAt, int _number) {
+        super("PullRequestReviewCommentEvent", _repoName, _createdAt);
+        this.number = _number;
+    }
+
+    public int getNumber() { return this.number; }
+
+    @Override
+    public String toString() {
+        return "Commented on pull request #" + this.getNumber() + " in " + this.getRepoName();
+    }
+}
