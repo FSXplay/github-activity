2e7f29e69aec3abb928f7ca7032a898c299e203a
FSXplay <skinoftheduck@gmail.com>
2026-02-18 20:51:05 +0700

    Convert a whole JSON into a list of GitHubEvent objects (Awaiting the implementation of convertToGitHubEvent())

--- src/services/JSONToGitHubEventConverter.java
+++ src/services/JSONToGitHubEventConverter.java
@@ -3,10 +3,39 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.GitHubEvents.GitHubEvent;
+
 /**
  * A utility class for converting JSON into GitHubEvent object
  */
 public class JSONToGitHubEventConverter {
+    /**
+     * Accept a whole JSON and return a list of GitHubEvent objects.
+     * @param json
+     * @return A list of GitHubEvent objects.
+     */
+    public static List<GitHubEvent> convertToGitHubEvents(String json) {
+        json = json.substring(1, json.length() - 2).trim();
+        List<GitHubEvent> events = new ArrayList<>();
+        String[] eventEntries = splitJsonEntries(json);
+
+        for (String eventData : eventEntries) {
+            GitHubEvent eventObject = convertToGitHubEvent(eventData);
+            events.add(eventObject);
+        }
+
+        return events;
+    }
+
+    /**
+     * Accept a JSON entry that represent a GitHub event, then convert it into a GitHubEvent object.
+     * @param eventData
+     * @return A GitHubEvent object.
+     */
+    public static GitHubEvent convertToGitHubEvent(String eventData) {
+
+    }
+
     /**
      * Splits a JSON array or a list of objects into individual entries based on
      * top-level commas.
@@ -17,7 +46,7 @@ public class JSONToGitHubEventConverter {
      * 
      * @return An array of Strings, where each element is a separate JSON entry.
      */
-    private static String[] splitJsonEntries(String json) {
+    public static String[] splitJsonEntries(String json) {
         List<String> entries = new ArrayList<>();
         int bracketCount = 0, braceCount = 0;
         boolean inQuotes = false;
