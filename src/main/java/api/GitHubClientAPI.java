4fcf00b2d9e14c6d952cf78143381239ee574387
FSXplay <skinoftheduck@gmail.com>
2026-02-21 14:19:53 +0800

    GET: compare two commits

--- src/main/java/api/GitHubClientAPI.java
+++ src/main/java/api/GitHubClientAPI.java
@@ -7,6 +7,7 @@ import java.net.URI;
 
 import services.JSONToGitHubEventConverter;
 import java.util.List;
+import org.json.JSONObject;
 
 import models.GitHubEvents.GitHubEvent;
 
@@ -26,6 +27,24 @@ public class GitHubClientAPI {
             return events;
         } catch (Exception e) {
             System.err.println("Error response");
+            return null;
+        }
+    }
+
+    public static JSONObject compareTwoCommits(String owner, String repo, String before, String head) {
+        String url = String.format("https://api.github.com/repos/%s/%s/compare/%s...%s", owner, repo, before, head);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Accept", "application/vnd.github+json")
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new JSONObject(response.body());
+        } catch (Exception e) {
+            System.err.println("Error fetching commit comparison: " + e.getMessage());
             return null;
         }
     }
